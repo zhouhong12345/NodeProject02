aab420be392b93074eb5dbe64b16d66bc0b0b8ab
zhouhong12345 <15041916476@163.com>
2020-03-18 17:56:27 +0800

    newest

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/*global module:true*/
+module.exports=function(grunt){
+  grunt.initConfig({
+    htmlmin:{
+      options:{
+        rmoveComments:true,
+        collapseWhitespace:true
+      },
+      files:{
+        src:'./index.html',
+        dest:'dist/index.html'
+      }
+    },
+    cssmin:{
+      'dist/enroll.css':'enroll.css'
+    },
+    uglify:{
+      'dist/enroll.js':'enroll.js'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default',['htmlmin','cssmin','uglify']);
+};
+
