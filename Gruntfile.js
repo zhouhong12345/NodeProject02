801c9c8b24aeb7e6fdf3307f26ca33fb61443031
zhouhong12345 <15041916476@163.com>
2020-03-18 18:56:34 +0800

    change

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,40 @@ module.exports=function(grunt){
   grunt.initConfig({
     htmlmin:{
       options:{
-        rmoveComments:true,
         collapseWhitespace:true
       },
       files:{
-        src:'./index.html',
+        src:'dist/index.html',
         dest:'dist/index.html'
       }
     },
     cssmin:{
-      'dist/enroll.css':'enroll.css'
+      'dist/enroll.css':'dist/enroll.css'
     },
     uglify:{
-      'dist/enroll.js':'enroll.js'
+      'dist/enroll.js':'dist/enroll.js'
+    },
+    copy:{
+       html:{
+         src:'./index.html',
+         dest:'./dist/index.html'
+       },
+       css:{
+         src:'./enroll.css',
+         dest:'./dist/enroll.css'
+       },
+       js:{
+         src:'./enroll.js',
+         dest:'./dist/enroll.js'
+       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('minify',['htmlmin','cssmin','uglify']);
+  grunt.registerTask('release',['copy','htmlmin','cssmin','uglify']);
 };
 
